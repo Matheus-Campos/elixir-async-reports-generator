e5005b3bbd0b95dd3b89fe6f4069d63e74e1e182
Matheus Campos da Silva <matheus.campos@mesainc.com.br>
2021-03-15 07:15:57 -0300

    chore: start project

---
+++ lib/async_reports_generator.ex
@@ -0,0 +1,18 @@
+defmodule AsyncReportsGenerator do
+  @moduledoc """
+  Documentation for `AsyncReportsGenerator`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> AsyncReportsGenerator.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/async_reports_generator_test.exs
@@ -0,0 +1,8 @@
+defmodule AsyncReportsGeneratorTest do
+  use ExUnit.Case
+  doctest AsyncReportsGenerator
+
+  test "greets the world" do
+    assert AsyncReportsGenerator.hello() == :world
+  end
+end
